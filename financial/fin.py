f5d2dbb5742950e74cbd301db15472ad536b85eb
Axel Beierlein <beierlein.axel@gmail.com>
2016-05-04 23:48:25 +0200

    - added function interest

--- financial/fin.py
+++ financial/fin.py
@@ -1,3 +1,8 @@
 
-def kaufkraftverlust(betrag, zins, jahre=1):
-    return(betrag * (1-(1/1+(zins/100.0)))**jahre)
+def kaufkraftverlust(betrag, inflation, jahre=1):
+    return(betrag * (-1/(1+(inflation/100.0)))**jahre)
+
+def zinszins(betrag, zins, jahre=1):
+    return(betrag*(1+(zins/100.0))**jahre)
+
+
