6a8cc1cf01751b115b88c823e747276ac6e87cb3
Belatronix <Belatronix@users.noreply.github.com>
2016-06-06 14:02:15 +0200

    - corrected Function kaufkraftverlust

--- financial/fin.py
+++ financial/fin.py
@@ -1,22 +1,21 @@
-
 def kaufkraftverlust(betrag, inflation, jahre=1):
-    return(betrag * (-1/(1+(inflation/100.0)))**jahre)
+    return (betrag / ((1+inflation/100)** jahre))
+
 
 def zinszins(betrag, zins, jahre=1):
-    return(betrag*(1+(zins/100.0))**jahre)
+    return betrag * (1 + (zins / 100.0)) ** jahre
+
 
 def einkommensteuer(zvE):
     if zvE <= 8652:
         return 0
-    elif zvE >= 8653 and zvE <= 13669:
-        y=(zvE-8652)/10000.0
+    elif 8653 <= zvE <= 13669:
+        y = (zvE - 8652) / 10000.0
         return (993.62 * y + 1400) * y
-    elif zvE >= 13670 and zvE <= 53665:
-        z=(zvE - 13669)/10000.0
+    elif 13670 <= zvE <= 53665:
+        z = (zvE - 13669) / 10000.0
         return (225.4 * z + 2397) * z + 952.48
-    elif zvE >= 53666 and zvE <= 254446:
+    elif 53666 <= zvE <= 254446:
         return 0.42 * zvE - 8394.14
-    elif zvE >= 254447:
+    elif 254447 <= zvE:
         return 0.45 * zvE - 16027.52
-
-
